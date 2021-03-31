79fe15184ee20ec80418d455d5df842e67c739da
Metisek <matboj5@gmail.com>
2021-03-31 16:01:33 +0200

    Granice mapy

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ let czujniki = xmlHttp.responseText;
 let sw = L.latLng(49.69600468606782, 22.40746406532194);
 let ne = L.latLng(49.85719699165818, 23.00415811475734);
 let bounds = L.latLngBounds(sw, ne);
-var mymap = L.map('mapid',{maxBouns: bounds}).setView([49.780145, 22.786583], 13);
+
+var mymap = L.map('mapid',{
+    maxBounds: bounds,
+    minZoom: 11,
+    maxZoom: 19
+}).setView([49.780145, 22.786583], 13);
 
 // Podkład topograficzny
 
@@ -24,8 +29,7 @@ var mymap = L.map('mapid',{maxBouns: bounds}).setView([49.780145, 22.786583], 13
 var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
-	minZoom: 11,
-    maxZoom: 19
+
 }).addTo(mymap);
 
 // Koniec podkładu
