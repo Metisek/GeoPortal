9b7ddf803e64d196bc4a83028ac96799a2452c54
Metisek <matboj5@gmail.com>
2021-03-31 15:42:05 +0200

    ok

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var mymap = L.map('mapid').setView([49.780145, 22.786583], 13);
 var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
-	minZoom: 13,
+	minZoom: 10,
     maxZoom: 19
 }).addTo(mymap);
 
